c2dac402d2267b1c1a4aa8d8ea784a4b2d48dcdd
João Saliba <joao_salibaa@hotmail.com>
2021-08-21 16:32:31 -0300

    views professor

--- rede_auth/views/user_views.py
+++ rede_auth/views/user_views.py
@@ -1,6 +1,6 @@
-from rede_auth.models import Student, User
+from rede_auth.models import Student, User, Teacher
 from rede_auth.views.mixed_view import MixedPermissionModelViewSet
-from rede_auth.serializers.user_serializer  import UserSerializer, StudentSerializer
+from rede_auth.serializers.user_serializer  import  TeacherSerializer, UserSerializer, StudentSerializer
 from rede_auth.permissions import *
 from rest_framework import viewsets
 from rest_framework.permissions import IsAuthenticated, IsAdminUser, AllowAny
@@ -20,6 +20,22 @@ class UserViewSet(viewsets.ModelViewSet):
 class StudentViewSet(MixedPermissionModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+
+    permission_classes_by_action = {'create': [AllowAny],
+                                    'list': [AllowAny],
+                                    'delete': [IsSameUser],
+                                    'update': [IsSameUser],
+                                    'partial_update': [IsSameUser]
+                                    }
+
+    def get_queryset(self):
+        if self.request.user.is_superuser:
+            return super().get_queryset()
+        return Student.objects.filter(id=self.request.user.id)
+
+class TeacherViewSet(MixedPermissionModelViewSet):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
 
     permission_classes_by_action = {'create': [AllowAny],
                                     'list': [AllowAny],
