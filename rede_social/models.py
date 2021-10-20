7aef930e7453737519272704f3dd2c20cd8ad82e
CaioGabrielAraujo <caiohgabriel@gmail.com>
2021-10-20 18:01:08 -0300

    fixing models

--- rede_social/models.py
+++ rede_social/models.py
@@ -1,3 +1,4 @@
+from __future__ import unicode_literals
 from django.db import models
 from django.db.models.signals import post_save
 from django.utils.encoding import force_bytes
@@ -5,7 +6,7 @@ from django.utils.translation import ugettext_lazy as _
 from django.dispatch import receiver
 from rede_auth.models import User
 from django.conf import settings
-from __future__ import unicode_literals
+
 import logging
 
 # Create your models here.
@@ -32,12 +33,12 @@ class Category(models.Model):
         return u'%s' % self.name
 
 class Post(models.Model):
-    category = models.ForeignKey(Category)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
     title = models.CharField(_('Title'), max_length=200)
     body = models.TextField(_('Body'), )
-    reply_to = models.ForeignKey('self', blank=True, null=True, related_name='child')
+    reply_to = models.ForeignKey('self', blank=True, null=True, related_name='child', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(getattr(settings, 'AUTH_USER_MODEL'), related_name='posts')
+    created_by = models.ForeignKey(getattr(settings, 'AUTH_USER_MODEL'), related_name='posts', on_delete=models.CASCADE)
 
     def __str__(self):
         # Use django.utils.encoding.force_bytes() because value returned is unicode
@@ -50,7 +51,7 @@ class Announcement(models.Model):
     title = models.CharField(_('Title'), max_length=200)
     body = models.TextField(_('Body'), )
     created_at = models.DateTimeField(auto_now_add=True)
-    created_by = models.ForeignKey(getattr(settings, 'AUTH_USER_MODEL'), related_name='announcers', editable=False)
+    created_by = models.ForeignKey(getattr(settings, 'AUTH_USER_MODEL'), related_name='announcers', editable=False, on_delete=models.CASCADE)
     announce_from = models.DateTimeField(_('Announce from'), null=True, blank=True)
     announce_to = models.DateTimeField(_('Announce to'), null=True, blank=True)
     mark_as_read = models.ManyToManyField(getattr(settings, 'AUTH_USER_MODEL'), related_name='announcements',
